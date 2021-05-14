59e005e748e1640741b955daf4e4f4c854747e93
Your Name <thongnhatgraphic@gmail.com>
2021-05-14 09:18:40 +0700

    update

--- nhat/js.js
+++ nhat/js.js
@@ -1,12 +1,1 @@
-var promise = function () {
-  return Promise.resolve("Đây là data");
-};
-
-function run(data) {
-  return data;
-}
-async function getData() {
-  var data = await promise();
-  console.log(run(data));
-}
-getData();
+console.log("Đây là file JS");
