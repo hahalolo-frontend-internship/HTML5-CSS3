d68e1f35e5b29155e0e0ee58e2631e0901bfbd54
baotheo1999 <nguyenquanganhbao148@gmail.com>
2021-06-10 21:47:47 +0700

    update layout1

--- dev-nguyenquanganhbao/layout1/src/index.js
+++ dev-nguyenquanganhbao/layout1/src/index.js
@@ -14,22 +14,24 @@ function Close() {
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     let sticky = document.getElementById("sticky");
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    let btnScroll = document.getElementById("btn-scroll-ontop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         sticky.style.backgroundColor = "#479d35";
         sticky.style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
         document.getElementById("header").style.height = "60px";
+        btnScroll.style.opacity = "1";
     } else {
         sticky.style.backgroundColor = "unset";
         sticky.style.boxShadow = "unset";
         document.getElementById("header").style.height = "126px";
+        btnScroll.style.opacity = "0";
     }
 }
 
-
 //active
 $(document).ready(function () {
-    $('.header-right__navmenu-list li').click(function () {
-        $('li').removeClass("active");
+    $('.header-right__navmenu-list li a').click(function () {
+        $('li a').removeClass("active");
         $(this).addClass("active");
     });
 });
@@ -37,26 +39,21 @@ $(document).ready(function () {
 
 
 //count
-function count() {
-    $(document).ready(function () {
-        $('.statistical-items__count').counterUp({
-            delay: 10,
-            time: 1500
-        });
+$(document).ready(function () {
+    $('.statistical-items__count').counterUp({
+        delay: 10,
+        time: 1500
     });
-}
-
-count();
+});
 
 
 //slick
-
 $('.variable-width').slick({
     dots: false,
     infinite: true,
-    speed: 300,
+    speed: 500,
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true,
-    // autoplay: true
+    autoplay: true
 });
