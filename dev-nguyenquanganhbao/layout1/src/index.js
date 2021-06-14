e0155ca1e2e69e74f556d8b4a3dcede4816539e7
baotheo1999 <nguyenquanganhbao148@gmail.com>
2021-06-14 08:57:01 +0700

    update layout1

--- dev-nguyenquanganhbao/layout1/src/index.js
+++ dev-nguyenquanganhbao/layout1/src/index.js
@@ -1,36 +1,34 @@
-function Open() {
-    document.getElementById("nav-menu").style.animation = "Open .5s forwards";
-    document.getElementById("overlay-nav-menu").style.visibility = "visible";
-    document.getElementById("body").style.overflowY = "hidden";
-}
-function Close() {
-    document.getElementById("nav-menu").style.animation = "Close .5s forwards";
-    document.getElementById("overlay-nav-menu").style.visibility = "collapse";
-    document.getElementById("body").style.overflowY = "unset";
-}
+//Open menu
+$( "#openMemu" ).click(function() {
+    $('#nav-menu').css('animation', 'Open .5s forwards');
+    $('.overlay-nav-menu').css('visibility', 'visible');
+    $('#body').css('overflowY', 'hidden');
+});
+//Close menu
+$( "#closeMenu" ).click(function() {
+    $('#nav-menu').css('animation', 'Close .5s forwards');
+    $('.overlay-nav-menu').css('visibility', 'collapse');
+    $('#body').css('overflowY', 'unset');
+});
 
-function scrolltop(){
+//click btn scroll top
+$( "#btn-scroll-ontop" ).click(function() {
     window.scroll(0, 0);
-}
-
+});
 
 //scroll
-window.onscroll = function () { scrollFunction() };
-function scrollFunction() {
-    let sticky = document.getElementById("sticky");
-    let btnScroll = document.getElementById("btn-scroll-ontop");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        sticky.style.backgroundColor = "#479d35";
-        sticky.style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
-        document.getElementById("header").style.height = "60px";
-        btnScroll.style.opacity = "1";
-    } else {
-        sticky.style.backgroundColor = "unset";
-        sticky.style.boxShadow = "unset";
-        document.getElementById("header").style.height = "126px";
-        btnScroll.style.opacity = "0";
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 20) {
+        $('#sticky').addClass('scroll-sticky');
+        $('#btn-scroll-ontop').css('opacity', '1');
     }
-}
+    else {
+        $('#sticky').removeClass('scroll-sticky');
+        $('#btn-scroll-ontop').css('opacity', '0');
+    }
+});
+
+
 
 //active
 $(document).ready(function () {
