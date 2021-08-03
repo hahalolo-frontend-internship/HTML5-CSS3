299b744a9d6422b8bba9fa519864445e16dda2b3
HoaCong <leconghoa11a1@gmail.com>
2021-08-03 08:27:12 +0700

    create redux for rank

--- leconghoa/process-math-react-route/src/App.js
+++ leconghoa/process-math-react-route/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import "./grid.scss";
 import Body from "./layouts/body/Body";
 import Footer from "./layouts/footer/Footer";
 import Header from "./layouts/header/Header";
+import { makeSelectIsSuccessLogin } from "./redux/selectors/login";
 
-export default function App() {
+function App(props) {
   return (
     <Router>
       <Header />
@@ -15,13 +18,13 @@ export default function App() {
         <Route
           path="/login"
           render={() =>
-            localStorage.getItem("isSignIn") ? <Body /> : <Login />
+            props.statusFlags.isLoginSuccess ? <Body /> : <Login />
           }
         />
         <Route
           path="/signup"
           render={() =>
-            localStorage.getItem("isSignIn") ? <Body /> : <SignUp />
+            props.statusFlags.isLoginSuccess ? <Body /> : <SignUp />
           }
         />
         <Route exact component={Body} />
@@ -30,3 +33,8 @@ export default function App() {
     </Router>
   );
 }
+const mapStateToProps = createStructuredSelector({
+  statusFlags: makeSelectIsSuccessLogin(),
+});
+
+export default connect(mapStateToProps, null)(App);
